6d3211b3680b31d0bd585ea18a0f56af62652644
abdelbassetben <a.benatmane@armonika.dz>
2026-05-30 14:37:21 +0100

    add main page and color page

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,12 +1,17 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps, LinkItemType } from 'fumadocs-ui/layouts/shared';
 import { appName, gitConfig } from './shared';
+
+const links: LinkItemType[] = [
+  { type: 'main', text: 'Colors', url: '/colors' },
+  { type: 'main', text: 'Get Started', url: '/docs/get-started' },
+];
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      // JSX supported
       title: appName,
     },
+    links,
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
 }
